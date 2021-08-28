3dfa29a8b9e6461d6bb0d3ebc11a26ad24547592
Nicole Anater <nicoleanater@gmail.com>
2021-08-27 22:39:07 -0300

    Editting import for the NotesApp.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React  from 'react';
 import './App.css';
 import 'h8k-components';
-import NotesApp from './components/notes-app/index.js';
+import NotesApp from './components/notes-app/NotesApp.js';
 
 const title = "Notes App";
 
