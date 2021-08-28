5ff294d4ca5a44ec9a7e4419b94b54fb18d2f3ed
Nicole Anater <nicoleanater@gmail.com>
2021-08-27 22:39:18 -0300

    Creating first component.

---
+++ src/components/notes-input/NotesInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NotesInput () {
+	return (
+    <input data-testid="input-note-name" type="text" className="large mx-8" placeholder="Note Title"/>
+	);
+}
+
+export default NotesInput;
